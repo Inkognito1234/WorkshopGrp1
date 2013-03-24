1c5127bb4c6f599bd4a557c4eebf9a5660a3a05e
Wotafak <h.weisenmueller@gmail.com>
2013-03-24 01:53:38 +0100

    lol

---
+++ Workshop/src/gruppe1/Main.java
@@ -0,0 +1,14 @@
+package gruppe1;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Check 1 2 1 2");
+
+	}
+
+}
