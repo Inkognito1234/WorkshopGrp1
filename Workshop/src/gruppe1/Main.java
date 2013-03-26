9de8a100303b73ee4899618b4baa6a2661baa848
Wotafak <h.weisenmueller@gmail.com>
2013-03-27 00:18:36 +0100

    ein weiterer test. kei luscht meh

--- Workshop/src/gruppe1/Main.java
+++ Workshop/src/gruppe1/Main.java
@@ -7,7 +7,9 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// hab nichts zu tun
 		System.out.println("Check 1 2 1 2");
+		System.out.println("332424");
 
 	}
 
